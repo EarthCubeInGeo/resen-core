b9630b72a4e93811023dc625dfa5b6c288560659
Leslie Lamarche <leslie.lamarche@sri.com>
2019-09-10 13:47:10 -0700

    Adding pyglow to docker image.

--- resen-core/resources/helpers/setup_pyglow.sh
+++ resen-core/resources/helpers/setup_pyglow.sh
@@ -18,33 +18,21 @@ cd $PYGLOW_BUILD_DIR
 git checkout 054bf800263c01ecfa783741c9cfe8d3d6c259ca -b master_20190904
 
 source /home/$NB_USER/envs/py27/bin/activate
+
 make -C src/pyglow/models source
-#cd src/pyglow/models
-#make all
-#cd ../../..
 pip install .
 python -c "import pyglow"
 
 deactivate
 
 source /home/$NB_USER/envs/py36/bin/activate
+
 make -C src/pyglow/models source
-#cd src/pyglow/models
-#make all
-#cd ../../..
 pip install .
 python -c "import pyglow"
 
-# some post-pip initialization of packages
-#cd $BUILD
-#source /usr/local/bin/definitions.B # to set the CDF definitios including $CDF_LIB
-#python -c "import spacepy.toolbox; spacepy.toolbox.update()"
-# a file about 195 MB called OMNI_OMNI2_merged_20120213-v1.cdf
-# has been created and will be removed later on. The data is kept
-# inside ~/.spacepy
-#echo "support_notice: False" >> /home/$NB_USER/.spacepy/spacepy.rc
+deactivate
 
-#deactivate
 
 # cleanup
 cd $BUILD
