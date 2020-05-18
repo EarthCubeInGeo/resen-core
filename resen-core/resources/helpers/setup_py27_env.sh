16a0a19b097deeb974790a01008f0203afeba8cf
Leslie Lamarche <leslie.lamarche@sri.com>
2020-05-18 13:22:24 -0700

    Update py36 package versions.

--- resen-core/resources/helpers/setup_py27_env.sh
+++ resen-core/resources/helpers/setup_py27_env.sh
@@ -42,7 +42,7 @@ pip install -U paramiko==2.4.2 \
                aacgmv2==2.5.2 \
                pymap3d==1.8.1 \
                astropy==2.0.14 \
-               sunpy==0.9.10
+               # sunpy==0.9.10
 
 # Custom pip installation for any package that needs it
 pip install apexpy==1.0.3  # have to install after installing numpy
