7489c8f9e6b1deb68fc07d91aa6cb0776a1b1437
Leslie Lamarche <leslie.lamarche@sri.com>
2020-06-01 11:05:39 -0700

    Add citationhelper and citation reference file.

---
+++ resen-core/resources/helpers/setup_citationhelper.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+######################################################################
+#
+#    A helper script for installing citationhelper
+#
+######################################################################
+
+echo "*** Installing citationhelper ***"
+
+source ${HOME}/envs/py27/bin/activate
+pip install git+https://github.com/EarthCubeInGeo/citationhelper.git@v0.1
+deactivate
+
+source ${HOME}/envs/py36/bin/activate
+pip install git+https://github.com/EarthCubeInGeo/citationhelper.git@v0.1
+deactivate
+
+echo "export CITEHELP_REFFILE=${HOME}/cache/pkg_citations.json" >> ${HOME}/.bashrc
