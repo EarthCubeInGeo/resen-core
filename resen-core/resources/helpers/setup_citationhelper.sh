62d0618b6b9516f946ae85be29ab200977089e3d
Leslie Lamarche <leslie.lamarche@sri.com>
2020-06-04 16:55:07 -0700

    Change version of citation helper to v0.2.

--- resen-core/resources/helpers/setup_citationhelper.sh
+++ resen-core/resources/helpers/setup_citationhelper.sh
@@ -8,11 +8,11 @@
 echo "*** Installing citationhelper ***"
 
 source ${HOME}/envs/py27/bin/activate
-pip install git+https://github.com/EarthCubeInGeo/citationhelper.git@v0.1
+pip install git+https://github.com/EarthCubeInGeo/citationhelper.git@v0.2
 deactivate
 
 source ${HOME}/envs/py36/bin/activate
-pip install git+https://github.com/EarthCubeInGeo/citationhelper.git@v0.1
+pip install git+https://github.com/EarthCubeInGeo/citationhelper.git@v0.2
 deactivate
 
 echo "export CITEHELP_REFFILE=${HOME}/cache/pkg_citations.json" >> ${HOME}/.bashrc
