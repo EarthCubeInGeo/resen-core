213c2b979a4edfca7faca5002b4000ca7c1b9456
Ashton Reimer <ashtonsethreimer@gmail.com>
2021-06-18 02:33:35 -0700

    fixed up so models install

--- resen-core/resources/helpers/setup_emp_models.sh
+++ resen-core/resources/helpers/setup_emp_models.sh
@@ -4,47 +4,22 @@
 #    A helper script for installing the igrf, iri2016, and msise00
 #    empirical models
 #
-#    Assumes you have a py38 virtual environment
+#    Assumes you are root and have a py38 virtual environment in /home/jovyan/envs
 #
 ####################################################################################
 
 echo "**** Installing IGRF, IRI2016, & MSISE00 ****"
 pwd
 
-EMPMOD_BUILD_DIR=empmod_build
-mkdir ${HOME}/${EMPMOD_BUILD_DIR}
+apt-get update && apt-get install -yq --no-install-recommends \
+    cmake && \
+    rm -rf /var/lib/apt/lists/*
 
-# install  cmake
-pwd
-cd ${HOME}/${EMPMOD_BUILD_DIR}
-pwd
-wget https://github.com/Kitware/CMake/releases/download/v3.19.6/cmake-3.19.6.tar.gz
-tar -xzf cmake-3.19.6.tar.gz && rm cmake-3.19.6.tar.gz
-cd cmake-3.19.6
-pwd
-./bootstrap
-make
-make install
-cd ${HOME}/${EMPMOD_BUILD_DIR}
-pwd
-# become jovyan user
-su jovyan
+# install igrf, iri, msise00
+su -l jovyan -c "source /home/jovyan/envs/py38/bin/activate && pip install -U igrf==13.0.0 iri2016==1.11.1 msise00==1.10.0"
+su -l jovyan -c 'source /home/jovyan/envs/py38/bin/activate && python -c "import igrf; mag = igrf.igrf(\"2010-07-12\", glat=65, glon=-148, alt_km=100)"'
+su -l jovyan -c 'source /home/jovyan/envs/py38/bin/activate && python -c "import iri2016.profile as iri; from datetime import datetime, timedelta; sim = iri.timeprofile((datetime(2020, 6, 1, 0, 0, 0), datetime(2020, 6, 2)), timedelta(minutes=30), (100, 500, 10.), 65, -147.5)"'
+su -l jovyan -c 'source /home/jovyan/envs/py38/bin/activate && python -c "import msise00; from datetime import datetime; atmos = msise00.run(time=datetime(2013, 3, 31, 12), altkm=150., glat=65., glon=-148.)"'
 
-# pip install igrf, iri, msise00
-source /home/jovyan/envs/py38/bin/activate
 
-pip install -U igrf==13.0.0 iri2016==1.11.1 msise00==1.10.0
-
-# import in python
-python -c "import igrf, iri2016, msise00"
-
-# become root
-su
-
-# uninstall cmake
-cd ${HOME}/${EMPMOD_BUILD_DIR}/cmake-3.19.6
-pwd
-make uninstall
-cd ${HOME}
-pwd
-rm -r $EMPMOD_BUILD_DIR
+apt-get remove -yq cmake && apt-get autoremove -yq
