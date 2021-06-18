d7d1743ba88acd13a864e623342b2888120cd8c1
Ashton Reimer <ashtonsethreimer@gmail.com>
2021-06-17 23:33:35 -0700

    updating to jupyterlab3, using prebuilt extensions that don't need npm and nodejs, restructuring pip install structure so that installation doesn't fail due to package weirdness.

--- resen-core/resources/helpers/setup_py38_env.sh
+++ resen-core/resources/helpers/setup_py38_env.sh
@@ -13,45 +13,47 @@ pip install pip==21.1.2
 # Now use pip to install everything we can
 # NOTES: pyproj==1.9.6 required for basemap, 2.0.0 breaks basemap
 #        jupyterhub, jupyterlab, notebook should all follow jupyterstacks versions
-pip install -U jupyterhub==1.1.0 \
-               jupyterlab==2.2.5 \
-               notebook==6.1.3 \
-               paramiko==2.7.2 \
-               ipython==7.23.1 \
-               pytest==6.2.4 \
-               nbformat==5.0.7 \
-               numpy==1.20.3 \
-               matplotlib==3.4.2 \
-               scipy==1.6.3 \
-               tables==3.6.1 \
-               sgp4==2.19 \
-               pandas==1.2.4 \
-               sympy==1.8 \
-               sqlalchemy==1.4.15 \
-               h5py==3.2.1 \
-               pytz==2021.1 \
-               netcdf4==1.5.6 \
-               bokeh==2.3.2 \
-               pyyaml==5.4.1 \
-               cython==0.29.23 \
-               pyproj==3.1.0 \
-               madrigalweb==3.2 \
-               bsddb3==6.2.9 \
-               aacgmv2==2.6.2 \
-               astropy==4.2.1 \
-               plasmapy==0.6.0 \
-               pydarn==2.0.1 \
-               viresclient==0.8.0 \
-               ipympl==0.7.0 \
-               hdfviewer==0.11.0 \
-               watermark==2.2.0 \
-               seaborn==0.11.1 \
-               ephem==3.7.7.1 \
-               skyfield==1.39 \
-               sciunit2==0.4.post58.dev224550607 \
-               pysat==3.0.0 \
-               plotly==4.14.3 \
-               xarray==0.18.2
+pip install -U jupyter-core==4.7.1
+pip install -U jupyterhub==1.1.0
+pip install -U jupyterlab==3.0.16
+pip install -U notebook==6.4.0
+pip install -U paramiko==2.7.2
+pip install -U ipython==7.24.1
+pip install -U pytest==6.2.4
+pip install -U nbformat==5.0.7
+pip install -U numpy==1.20.3
+pip install -U matplotlib==3.4.2
+pip install -U scipy==1.6.3
+pip install -U tables==3.6.1
+pip install -U sgp4==2.19
+pip install -U pandas==1.2.4
+pip install -U sympy==1.8
+pip install -U sqlalchemy==1.4.15
+pip install -U h5py==3.2.1
+pip install -U pytz==2021.1
+pip install -U netcdf4==1.5.6
+pip install -U bokeh==2.3.2
+pip install -U pyyaml==5.4.1
+pip install -U cython==0.29.23
+pip install -U pyproj==1.9.6
+pip install -U madrigalweb==3.2
+pip install -U bsddb3==6.2.9
+pip install -U aacgmv2==2.6.2
+pip install -U astropy==4.2.1
+pip install -U plasmapy==0.6.0
+pip install -U pydarn==2.1.0
+pip install -U viresclient==0.8.0
+pip install -U hdfviewer==0.11.0
+pip install -U watermark==2.2.0
+pip install -U seaborn==0.11.1
+pip install -U ephem==3.7.7.1
+pip install -U skyfield==1.39
+pip install -U sciunit2==0.4.post58.dev224550607
+pip install -U pysat==3.0.0
+pip install -U xarray==0.18.2
+
+# install jupyterlab prebuilt extension
+pip install ipywidgets==7.6.3 ipympl==0.7.0 jupyter_bokeh==3.0.2 jupyterlab_widgets==1.0.0
 
 #build matplotlib font cache
 MPLBACKEND=Agg python -uc "import matplotlib.pyplot"
@@ -63,6 +65,7 @@ pip install -U sunpy==3.0.0 beautifulsoup4==4.9.3 drms==0.6.2 zeep==4.0.0 tqdm==
 pip install apexpy==1.1.0  # have to install after installing numpy
 
 pip install OMMBV==0.5.4  # install after numpy
+python -uc "import OMMBV"
 
 pip install cartopy==0.19.0.post1 # have to install after installing numpy
 
@@ -73,13 +76,6 @@ pip install spacepy==0.2.2
 source /usr/local/bin/definitions.B # to set the CDF definitios including $CDF_LIB
 # only update omni and qin-denton since maia.usno.navy.mil/ser7/tai-utc.dat leapseconds website was down at the time...
 python -uc "import spacepy.toolbox; spacepy.toolbox.update(QDomni=True)"
-# spacepy 0.2.1 doesn't clean up this file. Submitted issue and PR to spacepy, see https://github.com/spacepy/spacepy/pull/219
-rm OMNI_OMNI2_merged_20120213-v1.cdf
-
-# # install mayavi
-# pip install mayavi==4.7.3 PyQt6==6.1.0
-# jupyter nbextension install --py mayavi --user
-# jupyter nbextension install --py mayavi --user
 
 # Installing mangopy (14 June 2018)
 pip install git+https://github.com/astib/MANGO.git@2dd4ca5380dca54cac8d2180c3ad63fc041a5c67
